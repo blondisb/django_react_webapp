265dc968c99424996e21d142a41509ff677d2d22
blondisb <blondisb@uninorte.edu.co>
2024-09-14 17:49:54 -0500

    fixing frontend form

--- frontend/src/components/Form.jsx
+++ frontend/src/components/Form.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import api from "../api";
-import useNavigate from "react-router-dom";
+import {useNavigate} from "react-router-dom";
 import { ACCESS_TOKEN, REFRESH_TOKEN } from "../constants";
 import "../styles/Form.css"
 
